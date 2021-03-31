7046668e45d44c3303a2f2c3341b740d83e2c98a
Vladimir Riz <resin.fhp@gmail.com>
2021-03-31 12:02:39 +0300

    feat: organize multiple tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,13 +2,24 @@ const { expect } = require('chai');
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is represent', function () {
-  const req = {
-    get: function () {
-      return null;
-    },
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    'Not authenticated'
-  );
+describe('Auth authorization', function () {
+  it('should throw an error if no authorization header is represent', function () {
+    const req = {
+      get: function () {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not authenticated'
+    );
+  });
+
+  it('should throw an error if the authorization header is only one string', function () {
+    const req = {
+      get: function () {
+        return 'null';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
