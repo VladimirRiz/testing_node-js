21237dfd34ae514d7a1943b357164a10e2461dd9
Vladimir Riz <resin.fhp@gmail.com>
2021-03-31 12:28:07 +0300

    feat: use stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const { expect } = require('chai');
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -18,6 +20,29 @@ describe('Auth authorization', function () {
     const req = {
       get: function () {
         return 'null';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function () {
+    const req = {
+      get: function () {
+        return 'Bearer xyz';
+      },
+    };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'abc' });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
+
+  it('should throw an error if the token cannot be verified', function () {
+    const req = {
+      get: function () {
+        return 'Bearer xyz';
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
